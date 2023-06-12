027a43f387076704ed469ddfdcb314a7b5990451
fasyaaa <muhammad.a.f.2210@gmail.com>
2023-06-12 14:49:59 +0700

    p

--- exepemdas3_177/exepemdas3_177.cpp
+++ exepemdas3_177/exepemdas3_177.cpp
@@ -1,20 +1,84 @@
-// exepemdas3_177.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+class bidangDatar {
+private : 
+	int x; //variabel untuk menyimpan input dari lingkaran maupun bujursangkar
+public :
+	bidangDatar() { //constructor
+		x=0;
+	}
+	virtual void input() {} // fungsi yang menerima input dan mengirim input melalui fungsi setX untuk disimpan di x
+	virtual float Luas(int a) { 
+		return 0; 
+	} //fungsi untuk menghitung luas
+	virtual float Keliling(int a) { 
+		return 0;
+	} //fungsi untuk menghitung keliling
+
+	void setX(int a) { //fungsi untuk memberi/mengirim nilai pada x
+		this->x = a;
+	}
+	int getX() { //fungsi untuk membaca/mengambil nilai dalam x
+		return x;
+	}
+};
+
+
+class Lingkaran :public bidangDatar { //pemakaian/penurunan class bidang datar (inheritance)
+public:
+	void input() {
+		int r;
+		cout << "Masukkan jejari : ";
+		cin >> r;
+		setX(r);
+	}
+
+	float Luas(int r) {
+		return 3.14 * r * r;
+	}
+
+	float Keliling(int r) {
+		return 2 * 3.14 * r;
+	}
+};
+
+class Bujursangkar :public bidangDatar { //pemakaian/penurunan class bidang datar (inheritance)
+public: 
+	void input() {
+		int s;
+		cout << "Masukkan sisi : ";
+		cin >> s;
+		setX(s);
+	}
+
+	float Luas(int s) {
+		return s * s;
+	}
+
+	float Keliling(int s) {
+		return 4 * s;
+	}
+};
+
+int main() {
+	bidangDatar* bd;
+
+	//LINGKARAN
+	cout << "Lingkaran dibuat" << endl;
+	bd = new Lingkaran();
+	bd->input();			//memanggil dari kelas bidang datar agar bisa digunakkan
+	int r = bd->getX();		//menggunakan fungsi untuk membaca/mengambil nilai dalam x pada kelas bidang datar
+	cout << "Luas Lingkaran = " << bd->Luas(r) << endl;		//menggunakan/memanggil fungsi luas pada lingkaran karena terdapat (r)
+	cout << "Keliling Lingkaran = " << bd->Keliling(r) << endl;		//memanggil/menggunakan fungsi keliling lingkaran karena terdapat (r)
+
+	//BUJURSANGKAR
+	cout << "\nBujursangkar dibuat" << endl;
+	bd = new Bujursangkar();
+	bd->input();
+	int s = bd->getX();
+	cout << "Luas Bujursangkar = " << bd->Luas(s) << endl;	//menggunakan fungsi Luas pada bujursangkar karena terdapat (s)
+	cout << "Keliling Bujursangkar = " << bd->Keliling(s) << endl;	//menggunajan fungsi Keliling pada bujursangkar karena terdapat (s)
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
